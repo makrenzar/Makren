9fd1a84ccde2671f415ab126b586a596885e505c
Mark Lawrence Zaragoza <markzaragoza301@gmail.com>
2024-04-16 02:16:13 +0800

    Added a Projects Section and small changes at Hero Section

--- src/components/Pages/HeroSerction.jsx
+++ src/components/Pages/HeroSerction.jsx
@@ -4,53 +4,53 @@ function HeroSection() {
       id: "html",
       src: "https://skillicons.dev/icons?i=html",
       className:
-        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14",
+        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 lg:w-12 lg:h-12",
     },
     {
       id: "css",
       src: "https://skillicons.dev/icons?i=css",
       className:
-        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14",
+        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 lg:w-12 lg:h-12",
     },
     {
       id: "js",
       src: "https://skillicons.dev/icons?i=js",
       className:
-        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14",
+        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 lg:w-12 lg:h-12",
     },
     {
       id: "react",
       src: "https://skillicons.dev/icons?i=react",
       className:
-        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14",
+        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 lg:w-12 lg:h-12",
     },
     {
       id: "tailwind",
       src: "https://skillicons.dev/icons?i=tailwind",
       className:
-        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14",
+        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 lg:w-12 lg:h-12",
     },
     {
       id: "bootstrap",
       src: "https://skillicons.dev/icons?i=bootstrap",
       className:
-        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14",
+        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 lg:w-12 lg:h-12",
     },
     {
       id: "figma",
       src: "https://skillicons.dev/icons?i=figma",
       className:
-        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14",
+        "hover:scale-125 ease-in duration-200 cursor-pointer w-10 h-10 lg:w-12 lg:h-12",
     },
   ];
 
   return (
     <>
-      <div className="mt-20 md:mt-60 grid grid-flow-cols justify-center items-center gap-y-4">
+      <div className="mt-20 lg:mt-60 grid grid-flow-cols justify-center items-center gap-y-4">
         <p className="text-foreground scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl py-6">
           Mark Lawrence Zaragoza
         </p>
-        <p className="text-foreground scroll-m-20 text-2xl font-normal tracking-tight">
+        <p className="text-foreground scroll-m-20 text-2xl font-normal tracking-tight bg-white text-black w-64 rotate-3 underline underline-offset-4">
           Frontend Developer
         </p>
         <p className="text-foreground font-thin mt-6">
@@ -58,8 +58,8 @@ function HeroSection() {
           startups to create engaging websites
         </p>
       </div>
-      <div className=" items-center justify-center mt-52 grid grid-cols-4  md:flex gap-8 md:gap-24">
-        <span className="text-sm md:text-sm  font-semibold  rounded-sm cursor-pointer">
+      <div className=" items-center justify-center mt-52 grid grid-cols-4  lg:flex gap-8 lg:gap-24">
+        <span className="text-sm lg:text-sm  font-semibold  rounded-sm cursor-pointer">
           Skills
         </span>
         {SkillIcons.map((icon) => (
