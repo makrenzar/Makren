eabac47074581e4ef4a034a98673aebc3a5201a6
makrenzjarr <markzaragoza301@gmail.com>
2024-04-17 15:03:02 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Pages/Navbar";
-import HeroSection from "./components/Pages/HeroSerction";
+import Hero from "./components/Pages/Hero";
 import Projects from "./components/Pages/Projects";
 import Experience from "./components/Pages/Experience";
 import Footer from "./components/Pages/Footer";
@@ -12,7 +12,7 @@ function App() {
       <div className="main-h-screen  overflow-hidden ">
         <div className="max-w-7xl mx-auto p-5">
           <Navbar></Navbar>
-          <HeroSection></HeroSection>
+          <Hero></Hero>
           <Projects></Projects>
           <Experience></Experience>
           <Contact></Contact>
