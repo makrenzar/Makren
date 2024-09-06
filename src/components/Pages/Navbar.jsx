ea2c1af4bc6b9ea7d3daa20ce0c83ec07ed64ecb
makrenzjarr <markzaragoza301@gmail.com>
2024-09-06 11:47:43 +0800

    update

--- src/components/Pages/Navbar.jsx
+++ src/components/Pages/Navbar.jsx
@@ -50,7 +50,7 @@ function Navbar() {
         </a>
 
         {/* NAVIGATION FOR LARGE SCREEN */}
-        <div className="hidden md:flex items-center gap-10 wow animate__animated animate__fadeInUp">
+        <div className="hidden md:flex flex-1 justify-center items-center gap-10 wow animate__animated animate__fadeInUp">
           {Tabs.map((tab) => (
             <a
               key={tab.id}
@@ -62,34 +62,57 @@ function Navbar() {
           ))}
         </div>
 
-        {/* NAVIGATION ICONS FOR MOBILE SCREEN */}
-        <div className="flex md:hidden items-center gap-6 wow animate__animated animate__fadeInUp">
-          {Tabs.map((tab) => (
-            <a
-              key={tab.id}
-              href={`#${tab.id}`}
-              className="flex flex-col items-center hover:scale-125 ease-in duration-200"
-            >
-              {/* ICONS */}
-              <box-icon name={tab.icon} size="sm" color="#434343"></box-icon>
-            </a>
-          ))}
-        </div>
-
-        {/* Resume Button */}
+        {/* Resume Button for Large Screens */}
         <button
           onClick={openResume}
-          className="text-sm font-bold border border-black   hover:bg-[#434343] hover:text-white rounded-md px-4 py-1 hover:scale-105 ease-in duration-200 wow animate__animated animate__fadeInRight"
+          className="text-sm font-bold border border-black hidden md:inline hover:bg-[#434343] hover:text-white rounded-md px-4 py-1 hover:scale-105 ease-in duration-200 wow animate__animated animate__fadeInRight"
         >
           Resume
         </button>
       </nav>
 
+      {/* Navigation Icons for Mobile Screen at the Bottom */}
+      <div className="md:hidden fixed bottom-0 left-0 right-0 bg-[#DFDFDF] border-t shadow-top">
+        <div className="flex justify-around py-2">
+          {/* Home Icon */}
+          <a
+            href="/"
+            className="flex flex-col items-center text-center hover:scale-125 ease-in duration-200"
+            aria-label="Home"
+          >
+            <box-icon name="home-alt" size="sm" color="#434343"></box-icon>
+            <span className="text-xs text-[#3E3E3E]">.home</span>
+          </a>
+
+          {Tabs.map((tab) => (
+            <a
+              key={tab.id}
+              href={`#${tab.id}`}
+              className="flex flex-col items-center text-center hover:scale-125 ease-in duration-200"
+            >
+              {/* ICONS */}
+              <box-icon name={tab.icon} size="sm" color="#434343"></box-icon>
+              <span className="text-xs text-[#434343]">{tab.name.replace('#', '.')}</span>
+            </a>
+          ))}
+
+          {/* Resume Icon for Mobile Screen */}
+          <a
+            onClick={openResume}
+            className="flex flex-col items-center text-center hover:scale-125 ease-in duration-200"
+            aria-label="Resume"
+          >
+            <box-icon name="file" size="sm" color="#434343"></box-icon>
+            <span className="text-xs text-[#3E3E3E]">.resume</span>
+          </a>
+        </div>
+      </div>
+
       {/* Arrow Up Button */}
       {showArrow && (
         <button
           onClick={scrollToTop}
-          className="scroll-up hidden md:block fixed bottom-5 right-20 "
+          className="scroll-up hidden md:block fixed bottom-5 right-20"
           aria-label="Scroll to top"
         >
           <box-icon name="chevron-up" size="lg" color="#434343"></box-icon>
