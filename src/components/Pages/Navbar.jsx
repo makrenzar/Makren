527df6374f80ab2717ab5b8cfaf73ba7e59596df
makrenzjarr <markzaragoza301@gmail.com>
2024-09-09 16:48:27 +0800

    updating the resume button

--- src/components/Pages/Navbar.jsx
+++ src/components/Pages/Navbar.jsx
@@ -23,7 +23,6 @@ function Navbar() {
     });
   };
 
-  // Show or hide arrow up button based on scroll position
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 200) {
@@ -40,7 +39,7 @@ function Navbar() {
   return (
     <>
       <nav className="container flex items-center justify-between py-4">
-        {/* Logo */}
+
         <a href="/">
           <img
             src={icon}
@@ -49,7 +48,7 @@ function Navbar() {
           />
         </a>
 
-        {/* NAVIGATION FOR LARGE SCREEN */}
+  
         <div className="hidden md:flex flex-1 justify-center items-center gap-10 wow animate__animated animate__fadeInUp">
           {Tabs.map((tab) => (
             <a
@@ -61,20 +60,54 @@ function Navbar() {
             </a>
           ))}
         </div>
+        
+        <button onClick={openResume} className="bg-slate-800 no-underline hidden md:inline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white  ease-in duration-200 wow animate__animated animate__fadeInRight">
+  <span className="absolute inset-0 overflow-hidden rounded-full">
+    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+  </span>
+  <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+    <span>
+      Resume
+    </span>
+    <svg
+      fill="none"
+      height="16"
+      viewBox="0 0 24 24"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.75 8.75L14.25 12L10.75 15.25"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      />
+    </svg>
+  </div>
+  <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+</button>
 
-        {/* Resume Button for Large Screens */}
-        <button
-          onClick={openResume}
-          className="text-sm font-bold border border-black hidden md:inline hover:bg-[#434343] hover:text-white rounded-md px-4 py-1 hover:scale-105 ease-in duration-200 wow animate__animated animate__fadeInRight"
-        >
-          Resume
-        </button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       </nav>
 
-      {/* Navigation Icons for Mobile Screen at the Bottom */}
       <div className="md:hidden fixed bottom-0 left-0 right-0 bg-[#DFDFDF] border-t shadow-top">
         <div className="flex justify-around py-2">
-          {/* Home Icon */}
+
           <a
             href="/"
             className="flex flex-col items-center text-center hover:scale-125 ease-in duration-200"
@@ -90,13 +123,13 @@ function Navbar() {
               href={`#${tab.id}`}
               className="flex flex-col items-center text-center hover:scale-125 ease-in duration-200"
             >
-              {/* ICONS */}
+        
               <box-icon name={tab.icon} size="sm" color="#434343"></box-icon>
               <span className="text-xs text-[#434343]">{tab.name.replace('#', '.')}</span>
             </a>
           ))}
 
-          {/* Resume Icon for Mobile Screen */}
+ 
           <a
             onClick={openResume}
             className="flex flex-col items-center text-center hover:scale-125 ease-in duration-200"
@@ -108,7 +141,7 @@ function Navbar() {
         </div>
       </div>
 
-      {/* Arrow Up Button */}
+
       {showArrow && (
         <button
           onClick={scrollToTop}
